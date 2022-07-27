519da0557c5cf89db0fcf7fbfd8189dc62a652fd
keimille <keith@mediavine.com>
2022-07-27 09:07:37 -0400

    adding cli functionality

---
+++ upchecker/cli.py
@@ -0,0 +1,28 @@
+# cli.py
+
+import argparse
+
+def read_user_cli_args():
+    """Handle the CLI arguments and options."""
+    parser = argparse.ArgumentParser(
+        prog="upchecker", description="check the availability of websites"
+    )
+    parser.add_argument(
+        "-u",
+        "--urls",
+        "-f",
+        "--input-file",
+        metavar="URLs",
+        type=str,
+        default=[],
+        help="enter one or more website URLs",
+    )
+    return parser.parse_args()
+
+def display_check_result(result, url, error=""):
+    """Display the result of a connectivity check."""
+    print(f'The status of "{url}" is:', end=" ")
+    if result:
+        print('"Online!" 👍')
+    else:
+        print(f'"Offline?" 👎 \n  Error: "{error}"')    
