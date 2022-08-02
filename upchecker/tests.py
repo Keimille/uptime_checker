3217da2a580edc2ebb7c15f944c906781c10b7c5
keimille <keith@mediavine.com>
2022-08-02 13:29:44 -0400

    add unit tests and github action config

--- upchecker/tests.py
+++ upchecker/tests.py
@@ -5,6 +5,10 @@ class TestGetWebsiteUrls(unittest.TestCase):
     def test_get_website_url_success(self):
         actual = site_is_online("astronomy.com")
         self.assertEqual(actual, True)
+    
+    def test_get_website_url_exception(self):
+        with self.assertRaises(Exception):
+            site_is_online("not a url")
 
 
 if __name__ == '__main__':
